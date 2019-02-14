49e548c19bfff1f751a32e8942b494e284989d9a
JavierND <Winte@DESKTOP-MLO4H24>
2019-02-13 23:06:44 -0600

    Agregar archivos de proyecto.

---
+++ Producto2.0/Datos.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producto
+{
+    class Datos
+    {
+        public string articulo;
+
+        public string Articulo
+        {
+            get { return articulo; }
+            set { articulo = value; }
+        }
+
+
+        public Datos()
+        {
+            articulo = "";       
+        }
+
+        public Datos(string art)
+        {
+            articulo = art;
+        }
+
+      
+        #region //Unidad del producto y Desplegar Cantidad
+        //Unidad del producto
+        int[] unidadCosto = new int[2];
+
+        public void Cantidad()
+        {
+            int x = 25;
+            for (int o = 0; o < unidadCosto.Length; o++)
+            {             
+                int y = 2;
+                //Cantidad de articulos
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.SetCursorPosition(x, 2 + o);
+                Console.Write(" - Cantidad del articulo: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(51, y + o);
+                unidadCosto[o] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void DesplegarCantidad()
+        {
+            for (int o = 0; o < unidadCosto.Length; o++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(30, 5 + o);
+
+                Console.Write(unidadCosto[o]);
+            }
+        }
+        //Total de unidad producidas
+        public void totales()
+        {
+            int acumulador = 0;
+            for (int o = 0; o < unidadCosto.Length; o++)
+            {
+                acumulador = acumulador + unidadCosto[o];
+            }
+            Console.SetCursorPosition(30, 8);
+            Console.Write(acumulador);
+        }
+
+        public void FactorDeCostos()
+        {
+            Random rnd = new Random();
+            double numero;
+            numero = rnd.Next(5, 16);
+
+            for (int o = 0; o < unidadCosto.Length; o++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(58, 5 + o);
+                Console.Write(unidadCosto[o] * (numero/100));
+            }
+        }
+
+        #endregion 
+
+        #region //Costo de los Articulos
+        int[] costoFijo = new int[3];
+
+        public void CostoArticulo()
+        {
+            int xx = 63;
+            for (int ca = 1; ca < costoFijo.Length; ca++)
+            {
+                int yy = 1;
+                //Costo de los articulos
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.SetCursorPosition(xx,1 + ca);
+                Console.Write(" - Costo del articulo: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(86, yy + ca);
+                costoFijo[ca] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        public void DesplegarArticulo()
+        {
+            for(int ca = 1; ca < costoFijo.Length; ca++)
+            {
+                Console.SetCursorPosition(82, 4 + ca);
+                Console.Write(costoFijo[ca]);
+            }
+        }
+
+        public void CostoDeProduccion()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Producto2.0/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Producto;
+
+namespace Producto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Practica Num. 1 --------El diseño se mostrara una vez completado los datos \n");
+            Datos productos = new Datos();
+            string[] Productos = new string[2];
+            string[] articulo = new string[2];
+            int i = 1;
+            //
+            string acumulador = "";
+
+
+            for (; i < Productos.Length; i++)
+            {
+                Console.WriteLine();
+
+                for (int j = 1; j <= articulo.Length; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write(" - Articulo: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    productos.articulo = Console.ReadLine();
+
+                    //Acumulador - Articulo
+                    acumulador = acumulador + productos.articulo + "\n";
+                }
+            }
+
+
+            productos.Cantidad();
+            
+            productos.CostoArticulo();
+            
+            //Costo de los articulos
+
+            Console.Clear();
+            Console.Write("------------------------------------------------------------------------------------------------------------------------");
+            Console.SetCursorPosition(50, 1);
+            //Titulo Costo de produccion
+            Console.WriteLine("COSTOS DE PRODUCCION");
+            Console.Write("------------------------------------------------------------------------------------------------------------------------");
+            Console.SetCursorPosition(5, 3);
+            Console.Write("Artículo");
+            Console.SetCursorPosition(23, 3);
+            Console.Write("Unidades Producidas");
+            Console.SetCursorPosition(52, 3);
+            Console.Write("Factor De Costos");
+            Console.SetCursorPosition(77, 3);
+            Console.Write("Costo Fijo");
+            Console.SetCursorPosition(97, 3);
+            Console.WriteLine("Costo De Produccion");
+            Console.Write("------------------------------------------------------------------------------------------------------------------------");
+            //Desplegar Articulos
+            Console.Write(acumulador);
+            //Unidades producidas
+            productos.DesplegarCantidad();
+            //Factor de costo
+            productos.FactorDeCostos();
+            //Costo fijo por el usuario
+            productos.DesplegarArticulo();
+            Console.WriteLine();
+            Console.Write("------------------------------------------------------------------------------------------------------------------------");
+            Console.SetCursorPosition(5, 8);
+            Console.Write("Totales: ");
+            productos.totales();
+
+            Console.ReadKey();
+
+        }
+
+
+        
+
+    }
+}
